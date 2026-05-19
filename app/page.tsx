0079a1227c8b1d9f43671fa59193f7abe93985c5
John <yohannes.h93@gmail.com>
2026-05-20 00:30:13 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,7 @@
-"use client";
+import React from "react";
 
-import React, { useEffect, useState } from "react";
-import { fetchPopularMovies, fetchPopularTV } from "../server";
-import MovieList from "../components/shared/MovieList";
+function page() {
+  return <div>page</div>;
+}
 
-const HomePage = () => {
-  const [trending, setTrending] = useState([]);
-  const [popularMovies, setPopularMovies] = useState([]);
-  const [popularTV, setPopularTV] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // const trendingData = await fetchTrending();
-        const popularMoviesData = await fetchPopularMovies();
-        const popularTVData = await fetchPopularTV();
-
-        // setTrending(trendingData);
-        setPopularMovies(popularMoviesData);
-        setPopularTV(popularTVData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div className="homepage">
-      <section>
-        <h2>Trending</h2>
-        <MovieList movies={trending} />
-      </section>
-      <section>
-        <h2>Popular Movies</h2>
-        <MovieList movies={popularMovies} />
-      </section>
-      <section>
-        <h2>Popular TV Series</h2>
-        <MovieList movies={popularTV} />
-      </section>
-    </div>
-  );
-};
-
-export default HomePage;
+export default page;
